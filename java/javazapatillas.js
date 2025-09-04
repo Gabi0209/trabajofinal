102a7f5eec77e10ed6b7cf8aabb1188960437dfd
Gabi0209 <gabriela0209m@gmail.com>
2025-09-04 15:50:25 -0300

    Primer commit

---
+++ java/javazapatillas.js
@@ -0,0 +1,40 @@
+/*Menu hamburguesa*/
+
+let boton=document.getElementById("boton-menu");
+let menu=document.getElementById("menu-hamburguesa");
+
+boton.addEventListener("click",function(){
+menu.classList.toggle("menu-hamburguesa");
+menu.classList.toggle("quitar");
+
+})
+
+
+/*Modo oscuro*/
+
+const cambiarFondo=document.getElementById('cambiarFondo');
+const body=document.body;
+
+body.classList.add('tema-claro');
+
+cambiarFondo.addEventListener('click',function(){
+const icono=cambiarFondo.querySelector('i');
+if(body.classList.contains('tema-claro')){
+body.classList.remove('tema-claro');
+body.classList.add('tema-oscuro');
+icono.className="bi bi-brightness-high-fill";
+cambiarFondo.innerHTML='<i class="bi bi-brightness-high-fill"></i>';
+
+
+}
+else{
+body.classList.remove('tema-oscuro');
+body.classList.add('tema-claro');
+icono.className="bi bi-moon-stars-fill";
+cambiarFondo.innerHTML='<i class="bi bi-moon-stars-fill"></i>';
+
+
+}
+}
+);
+
